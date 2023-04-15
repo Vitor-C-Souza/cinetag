d70081c5447a3ed67fe0d3ee5538649c3526cf2d
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-15 12:15:10 -0300

    Adicionado um Container a todas as rotas e criado uma nova trocado de lugar onde o Cabeçalho e o Rodape é chamado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,5 @@
 import Banner from "components/Banner/index.js";
-import Cabecalho from "components/Cabecalho/";
 import Card from "components/Card/index.js";
-import Rodape from "components/Rodape/index.js";
 import Titulo from "components/Titulo/index.js";
 import Videos from "json/db.json";
 import React from "react";
@@ -10,7 +8,7 @@ import styles from './Inicio.module.css'
 export default function Inicio() {
   return (
     <>
-      <Cabecalho />
+      
       <Banner imagem="home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
@@ -20,7 +18,7 @@ export default function Inicio() {
           return <Card {...video} key={video.id} />
         })}
       </section>
-      <Rodape />
+      
     </>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
+import Cabecalho from "components/Cabecalho/index.js";
+import Container from "components/Container/index.js";
+import Favoritos from "components/Favoritos/index.js";
+import Rodape from "components/Rodape/index.js";
+
 const { default: Inicio } = require("./pages/Inicio/index.js");
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
 
 export function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Cabecalho />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </Container>
+      <Rodape />
+    </BrowserRouter>
+  );
 }

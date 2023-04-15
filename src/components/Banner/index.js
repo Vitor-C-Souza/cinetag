0ff59bf2d761690730a4cf7ba6bb8c8c82d5a883
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-15 10:33:45 -0300

    formatado os documentos

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,13 +1,14 @@
-import styles from './Banner.module.css'
+import styles from "./Banner.module.css";
 
-import React from 'react'
+import React from "react";
 
-export default function Banner({imagem}) {
+export default function Banner({ imagem }) {
   return (
-    <div 
-        className={styles.capa}
-        style={{ backgroundImage: `url('/imagens/banner-${imagem}.png')`}}
+    <div
+      className={styles.capa}
+      style={{ backgroundImage: `url('/imagens/banner-${imagem}.png')` }}
     >
-        Banner</div>
-  )
+      Banner
+    </div>
+  );
 }

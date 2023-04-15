6a035c89c76197443cdbd25714ad265af284d9be
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-15 11:01:33 -0300

    adicionado o componente card a pagina inicio

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner/index.js";
 import Cabecalho from "components/Cabecalho/";
+import Card from "components/Card/index.js";
 import Rodape from "components/Rodape/index.js";
 import Titulo from "components/Titulo/index.js";
 import React from "react";
@@ -12,6 +13,7 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card id='1' titulo='Goku' capa='https://i.pinimg.com/564x/c7/4c/a6/c74ca6827eed655aa080143a0970242f.jpg' />
       <Rodape />
     </>
   );

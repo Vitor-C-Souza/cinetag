2be4cc43c0389633c90212b5b28af325f2652377
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-15 10:19:15 -0300

    rodape desenvolvido e implementado no site

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,10 @@
+import styles from "./Rodape.module.css";
+import React from "react";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <h3>Desenvolvido por Alura.</h3>
+    </footer>
+  );
+}

921f9520309587d6db2b011b681d501520fc1f44
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-15 16:17:05 -0300

    ajuste da tag card para export default

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,24 +1,22 @@
 import Banner from "components/Banner/index.js";
-import { Card } from "components/Card/index.js";
+import Card from "components/Card/index.js";
 import Titulo from "components/Titulo/index.js";
 import Videos from "json/db.json";
 import React from "react";
-import styles from './Inicio.module.css'
+import styles from "./Inicio.module.css";
 
 export default function Inicio() {
   return (
     <>
-      
       <Banner imagem="home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
       <section className={styles.container}>
         {Videos.map((video) => {
-          return <Card {...video} key={video.id} />
+          return <Card {...video} key={video.id} />;
         })}
       </section>
-      
     </>
   );
 }

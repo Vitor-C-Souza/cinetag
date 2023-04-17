f78d4aff1a17fd09f74a5973ca82576fa94686ed
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-16 22:17:51 -0300

    adicionado o link ao card para a rota do player de video

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,15 +3,18 @@ import React from "react";
 import iconeFavoritar from "./favoritar.png";
 import iconeDesfavoritar from "./desfavoritar.png";
 import { useFavoritoContext } from "Contextos/Favoritos.js";
+import { Link } from "react-router-dom";
 
 export default function Card({ id, titulo, capa }) {
   const { favorito, adicionarFavorito } = useFavoritoContext();
-  const ehFavorito = favorito.some((fav) => fav.id === id)
-  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar
+  const ehFavorito = favorito.some((fav) => fav.id === id);
+  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
   return (
     <div className={styles.container}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
+      <Link className={styles.link} to={`/${id}`}>
+        <img src={capa} alt={titulo} className={styles.capa} />
+        <h2>{titulo}</h2>
+      </Link>
       <img
         src={icone}
         alt="Favoritar filme"

62ea4eab7863bd62cd5323fce562f01be8ed7eed
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-16 22:29:37 -0300

    adicionado a rota de player de video e o de pagina não encontrada

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import Container from "components/Container/index.js";
 import Favoritos from "pages/Favoritos/index.js";
 import Rodape from "components/Rodape/index.js";
 import FavoritosProvider from "Contextos/Favoritos.js";
+import Player from "pages/Player/index.js";
+import NaoEncontrado from "pages/NaoEncontrado/index.js";
 
 const { default: Inicio } = require("./pages/Inicio/index.js");
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
@@ -16,6 +18,8 @@ export function AppRoutes() {
           <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/favoritos" element={<Favoritos />} />
+            <Route path="/:id" element={<Player />} />
+            <Route path="*" element={<NaoEncontrado />} />
           </Routes>
         </FavoritosProvider>
       </Container>

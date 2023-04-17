4edadca6f023da73797e2bce9e66695cd3fcc39f
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-17 00:20:37 -0300

    altera a variavel videos para usar o my server json

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,18 @@
 import Banner from "components/Banner/index.js";
 import Card from "components/Card/index.js";
 import Titulo from "components/Titulo/index.js";
-import Videos from "json/db.json";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Inicio.module.css";
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/Vitor-C-Souza/cinetag-api/videos')
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
   return (
     <>
       <Banner imagem="home" />
@@ -13,7 +20,7 @@ export default function Inicio() {
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
       <section className={styles.container}>
-        {Videos.map((video) => {
+        {videos.map((video) => {
           return <Card {...video} key={video.id} />;
         })}
       </section>
